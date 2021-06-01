0f6fa158b974dea0ed6f063b76458f0e3c963b03
Jorge Villasmil <keine1lust@gmail.com>
2021-06-01 17:19:26 -0400

    add company to register

--- resources/views/layouts/_menus.blade.php
+++ resources/views/layouts/_menus.blade.php
@@ -33,7 +33,7 @@
                 <span class="ml-4">Cards</span>
             </a>
         </li>
-        <li class="relative px-6 py-3">
+        {{-- <li class="relative px-6 py-3">
             {!! request()->routeIs('charts') ? '<span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg" aria-hidden="true"></span>' : '' !!}
             <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200" href="{{route('charts')}}">
                 <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
@@ -42,7 +42,7 @@
                 </svg>
                 <span class="ml-4">Charts</span>
             </a>
-        </li>
+        </li> --}}
         <li class="relative px-6 py-3">
             {!! request()->routeIs('buttons') ? '<span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg" aria-hidden="true"></span>' : '' !!}
             <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200" href="{{route('buttons')}}">
