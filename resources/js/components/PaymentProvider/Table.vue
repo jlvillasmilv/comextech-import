b9893714340442e31b503063a1c662ce1c285adb
mauchacon99 <53098149+mauchacon99@users.noreply.github.com>
2021-06-07 08:34:16 -0400

    table-financing

--- resources/js/components/PaymentProvider/Table.vue
+++ resources/js/components/PaymentProvider/Table.vue
@@ -5,7 +5,7 @@
                         <table class="w-full whitespace-no-wrap">
                             <thead>
                                 <tr
-                                    class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                                    class="text-xs font-semibold tracking-wide text-left text-white  uppercase border-b dark:border-gray-700 bg-blue-900 dark:text-gray-400 dark:bg-gray-800">
                                     <th class="px-4 py-3">Pago</th>
                                     <th class=""> Porcentaje</th>
                                     <th class="px-4 py-3">Moneda</th>
@@ -272,7 +272,6 @@
                         </table>
                     </div>
             </div>
-           
         </div>
 </template>
 
