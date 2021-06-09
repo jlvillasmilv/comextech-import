7eb816d6a819b5e3fa39519b37685a9d0415717a
Jorge Villasmil <keine1lust@gmail.com>
2021-06-09 12:30:58 -0400

    Apllication controller & currencies api get

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,9 @@ Route::get('/', function () {
 Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
     
     Route::resource('supplier',  'App\Http\Controllers\Web\SupplierController');
+    Route::resource('applications',  'App\Http\Controllers\Web\ApplicationController');
+
+    Route::resource('company',  'App\Http\Controllers\Web\CompnayController');
   
     Route::get('/imports', [ImportsController::class, 'index'])->name('imports.index');
 
