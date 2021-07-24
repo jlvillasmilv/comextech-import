9e624b0eb40ead14cdacf70c9af33f5622dcd361
mauchacon99 <53098149+mauchacon99@users.noreply.github.com>
2021-07-24 14:46:25 -0400

    e-commerce payment

--- resources/js/pages/Import.vue
+++ resources/js/pages/Import.vue
@@ -70,9 +70,9 @@
             <Container :bg="false" v-if="activetab == 'Pago Proveedor'">
                 <FormPayment
                     :application_id="form.application_id"
-                    :amountTotal="form.amount"
                     @incomingMenu="incomingMenu"
                     :currencies="currency"
+                    :dataApplications="form"
                 />
             </Container>
 
