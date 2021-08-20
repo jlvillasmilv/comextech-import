a12b693a26391d848c25328cf7aad067b89e2456
Jorge <keine1lust@gmail.com>
2021-08-20 16:55:03 -0400

    add currencyconverterapi

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -23,7 +23,22 @@ class ServicesController extends Controller
     {
         return view('services.edit');
     }
-    
 
-   
+    public function convertCurrency($amount,$from_currency,$to_currency){
+      $apikey = env('API_KEY_CURRENCY');
+
+      $from_Currency = urlencode($from_currency);
+      $to_Currency = urlencode($to_currency);
+      $query =  "{$from_Currency}_{$to_Currency}";
+
+      $json = file_get_contents("https://free.currconv.com/api/v7/convert?q={$query}&compact=ultra&apiKey={$apikey}");
+      $obj = json_decode($json, true);
+
+      $val = floatval($obj["$query"]);
+
+
+      $total = $val * $amount;
+      return number_format($total, 2, '.', '');
+    }
+
 }
--- resources/js/components/Exchange/index.vue
+++ resources/js/components/Exchange/index.vue
@@ -18,7 +18,10 @@
     
         <div class="w-full overflow-hidden rounded-lg shadow-xs">
             <div class="w-full overflow-x-auto">
-                <table class="w-full table-fixed">
+                <div class="flex space-x-4 ...">
+                      <div class="w-4/5">
+
+                         <table class="w-full table-fixed">
                     <thead>
                         <tr
                             class="text-xs font-semibold tracking-wide text-left text-white  uppercase border-b dark:border-gray-700 bg-blue-900 dark:text-gray-400 dark:bg-gray-800"
@@ -27,30 +30,14 @@
                             <th class="w-1/4"> FECHA </th>
                             <th class="w-1/4"> MONEDA ORIGEN (M.O.) </th>
                             <th class="w-1/4"> MONTO M.O. </th>
-                            <th class="w-1/4"> MONTO CLP </th>
                         </tr>
                     </thead>
                     <tbody
                         class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
                     >
-                        <tr v-show="$store.getters.findService('ICS01')">
-                            <td class="text-left px-4 py-3 ">
-                                <div class="text-sm">
-                                    <div>
-                                        <p class="font-semibold input">
-                                            Pago al Proveedor
-                                        </p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="text-left px-4 py-3"></td>
-                            <td class="text-left px-4 py-3"></td>
-                            <td class="text-left px-4 py-3">
-                                {{ $store.state.currency.code }}
-                            </td>
-                        </tr>
+                      
                         <tr
-                            v-for="(item, key) in $store.state.payment"
+                            v-for="(item, key) in data"
                             :key="key"
                             class="text-gray-700 dark:text-gray-400"
                         >
@@ -58,105 +45,67 @@
                                 <div class="text-xs">
                                     <div>
                                         <p class="font-semibold input">
-                                            Pago al Proveedor Nro
-                                            {{ key + 1 }}
+                                            {{ item.name }}
                                         </p>
                                     </div>
                                 </div>
                             </td>
                             <td class="text-left px-4 py-3">
-                                {{ item.datePay }}
+                                {{ getHumanDate(item.date) }}
                             </td>
                             <td class="text-left px-4 py-3">
-                                {{
-                                    item.percentage *
-                                        $store.state.application.amount
-                                }}
+                                {{ item.currency }}
                             </td>
                             <td class="text-left px-4 py-3">
-                                 {{ $store.state.currency.code }}
+                                 {{ Number(item.amo).toLocaleString() }}
                             </td>
                         </tr>
-                        <tr v-show="$store.state.expenses.iva">
-                            <td class="text-left px-4 py-3">
-                                <div class="text-sm">
-                                    <div>
-                                        <p class="font-semibold input">
-                                            IVA Internacion
-                                        </p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="text-left px-4 py-3">
-                                <div class="text-sm">
-                                    <div>
-                                        <p class="font-semibold input">
-                                             
-                                        </p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="text-left px-4 py-3">
-                                <div class="text-sm">
-                                    <div>
-                                        <p class="font-semibold input">
-                                           {{ $store.getters.CIF * 19 }}
-                                        </p>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr v-show="$store.state.expenses.adv">
-                            <td class="text-left px-4 py-3">
-                                <div class="text-sm">
-                                    <div>
-                                        <p class="font-semibold input">
-                                            Aranceles
-                                        </p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="text-left px-4 py-3">
-                                <div class="text-sm">
-                                    <div>
-                                        <p class="font-semibold input">
-                                            
-                                        </p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="text-left px-4 py-3">
-                                <div class="text-sm">
-                                    <div>
-                                        <p class="font-semibold input">
-                                            {{ $store.getters.CIF * 6 }}
-                                        </p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="text-left px-4 py-3">
-                                <div class="text-sm">
-                                    <div>
-                                        <p class="font-semibold input">
-                                            
-                                        </p>
-                                    </div>
-                                </div>
+                
+                    </tbody>
+                   
+                </table>
+
+            </div>
+  <div class="w-1/5">
+      
+         <table class="w-full table-fixed">
+                    <tr
+                            class="text-xs font-semibold tracking-wide text-left text-white  uppercase border-b dark:border-gray-700 bg-blue-900 dark:text-gray-400 dark:bg-gray-800"
+                        >
+                        <th class="w-1/2 px-4 py-3 text-center">Monto {{ currency_ex }}</th>
+                    </tr>
+                   <tbody
+                        class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
+                    >
+                        <tr  v-for="(item, key) in data"
+                            :key="key"
+                            class="text-gray-700 dark:text-gray-400">
+                             <td class="px-4 py-3 text-center">
+                                 {{  Number(item.amo2).toLocaleString() }}
                             </td>
                         </tr>
                     </tbody>
-                    <tfoot>
+                     <tfoot>
                         <tr>
-                            <td colspan="4"></td>
-                            <td>{{ totalAmount }}</td>
+                            <td class="text-center px-4 py-3"> <strong>{{  Number(totalAmount).toLocaleString() }}</strong></td>
                         </tr>
                     </tfoot>
-                </table>
+         </table>
+
+  </div>
+ 
+</div>
+               
+
+               
             </div>
         </div>
     </div>
 </template>
 <script>
+
+const moment = require('moment');
+
 export default {
     props: {
         application_id: {
@@ -169,50 +118,123 @@ export default {
             form: new Form({
                 application_id: this.application_id
             }),
+            currency_ex: 'CLP',
             data: [
                 {
+                    id: 1,
                     name: "A. pago proveedor",
-                    date: "",
-                    mo: "YUAN",
+                    date: "2021-08-21",
+                    currency: "CNY",
                     amo: 455000,
-                    clp: 6000000
+                    amo2: 6000000
                 },
                 {
+                    id: 2,
                     name: "A.1 ADELANTO",
                     date: "2021-08-21",
-                    mo: "YUAN",
+                    currency: "CNY",
                     amo: 455000,
-                    clp: 6000000
+                    amo2: 6000000
                 },
                 {
+                    id: 3,
                     name: "A.1 ADELANTO",
                     date: "2021-08-21",
-                    mo: "YUAN",
+                    currency: "CNY",
                     amo: 330000,
-                    clp: 4000000
+                    amo2: 4000000
                 },
                 {
+                    id: 4,
                     name: "B.- Transporte Internacional",
                     date: "2021-10-10",
-                    mo: "USD",
+                    currency: "USD",
                     amo: 4000,
-                    clp: 3000000
-                }
+                    amo2: 3000000
+                },
+                {
+                    id: 5,
+                    name: "C.- Seguro Transporte",
+                    date: "2021-10-10",
+                    currency: "USD",
+                    amo: 300,
+                    amo2: 210000
+                },
+                {
+                    id: 6,
+                    name: "D.- Servicio AGA",
+                    date: "2021-10-30",
+                    currency: "CLP",
+                    amo: 250,
+                    amo2: 3000000
+                },
+                {
+                    id: 7,
+                    name: "E.- IVA Internacion",
+                    date: "2021-10-30",
+                    currency: "CLP",
+                    amo: 2000000,
+                    amo2: 2000000
+                },
+                {
+                    id: 8,
+                    name: "F.- Aranceles",
+                    date: "2021-10-30",
+                    currency: "CLP",
+                    amo: 1000000,
+                    amo2: 1000000
+                },
+                {
+                    id: 9,
+                    name: "F.- Transporte Local",
+                    date: "2021-11-10",
+                    currency: "CLP",
+                    amo: 300000,
+                    amo2: 300000
+                },
             ]
         };
     },
     methods: {
-        convert(currency) {
-            console.log(currency);
+        getHumanDate(date) {
+            return moment(date, 'YYYY-MM-DD').format('DD-MM-YY');
+        },
+         convert(currency) {
+            
+            this.currency_ex = currency;
+
+            this.data.forEach(async e => {
+
+                let currencies_api = e.currency+'_'+currency; 
+                var new_amo2 = e.amo;
+
+                try {
+                  const resp = await axios.get('/api/convert-currency/'+e.amo+'/'+e.currency+'/'+currency);
+
+                    //Find index of specific object using findIndex method.    
+                   let objIndex = this.data.findIndex((obj => obj.id == e.id));
+                    
+                   //Update object's name property.
+                   this.data[objIndex].amo2 = resp.data;
+
+                    console.log(resp.data);
+                } catch (err) {
+                    // Handle Error Here
+                    console.error(err);
+                }
+
+
+            });
+            // 
         }
     },
     computed: {
         totalAmount: function() {
             var sum = 0;
             this.data.forEach(e => {
-                sum += e.clp;
+                sum += e.amo2;
             });
-            return sum;
+            return  Number(sum);
         }
     }
 };
