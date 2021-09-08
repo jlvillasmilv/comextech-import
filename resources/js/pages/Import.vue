d05e646ed82e5894f939a8a07b5a10ad942568ff
mauchacon99 <mauchacon99@gmail.com>
2021-09-08 09:46:27 -0400

    reset payments when changing percentages

--- resources/js/pages/Import.vue
+++ resources/js/pages/Import.vue
@@ -127,7 +127,7 @@
                                     'md:mb-0'
                                 ]"
                             >
-                                <h3 class="my-3  text-gray-500  text-sm">
+                                <h3 class="my-2.5  text-gray-500  text-sm">
                                     Condicion de Venta
                                 </h3>
                                 <div class="relative">
@@ -180,7 +180,7 @@
                                         item == data.valuePercentage
                                             ? 'bg-blue-500 text-white '
                                             : 'bg-transparent text-blue-700 ',
-                                        'hover:bg-blue-500 font-semibold hover:text-white py-1 mx-0.5 border border-blue-500 hover:border-transparent rounded my-2 text-center' 
+                                        'hover:bg-blue-500 font-semibold hover:text-white px-1 py-2 text-sm mx-0.5 border border-blue-500 hover:border-transparent rounded my-2 text-center' 
                                     ]"
                                 >   
                                    {{ item.name }}
@@ -419,6 +419,7 @@ export default {
             }
         },
         handlePercentage(item) {
+            this.handleCurrency();
             this.data.valuePercentage = item
         },
         handleCurrency(){
@@ -444,14 +445,10 @@ export default {
             this.$store.dispatch('application/getSuppliers')
             this.$store.dispatch('application/getServices')
             this.$store.dispatch('application/getCurrencies')
-           
-
         } catch (error) {
             console.log(error);
         }
-
         let application = document.getElementById("applications")
-
         if (application == null) {
             console.log("Nueva Solicitud")
         } else {
