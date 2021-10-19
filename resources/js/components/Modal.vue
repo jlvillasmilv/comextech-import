ed41c64af961922b29eaad3c7f0127ab774a52e8
Jorge <keine1lust@gmail.com>
2021-10-18 22:15:06 -0400

    responsive modal

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -1,15 +1,10 @@
 <template>
-  <div class="h-screen w-full fixed left-1 top-0 flex justify-center items-center bg-black bg-opacity-50">
+  <div class="overflow-x-hidden h-screen w-full fixed left-1 top-0 flex justify-center items-center bg-black bg-opacity-50">
         <div class="w-full px-3 py-2 overflow-hidden bg-white md:max-w-xl rounded-t-lg dark:bg-gray-800 sm:rounded-lg sm:m-4 sm:max-w-xl" role="dialog" id="modal">
             <header class="flex justify-between">
                  <p class="mb-1 text-lg   text-gray-400 dark:text-gray-300">
                     {{ title }}
                 </p>
-                <button  class="inline-flex items-center justify-center w-6 h-6 text-gray-400 transition-colors duration-150 rounded dark:hover:text-gray-200 hover: hover:text-gray-700" aria-label="close"  >
-                    <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" role="img" aria-hidden="true">
-                        <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" fill-rule="evenodd"></path>
-                    </svg>
-                </button>
             </header>
             <!-- Modal body -->
             <div class="mt-1 mb-1">
