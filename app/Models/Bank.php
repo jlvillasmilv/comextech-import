6b9ff6716ce7a61d7c619143cda99abe0238ce8f
Jorge Villasmil <keine1lust@gmail.com>
2021-10-29 11:58:48 -0400

    add bank account client

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bank extends Model
 {
-   
+    protected $fillable = [
+        'name', 'rut', 'observation', 'status'
+    ];
+
+    protected $table = 'banks';
+
+    /**
+     * Get the bankAccounts associated with the bank.
+     */
+    public function bankAccounts()
+    {
+        return $this->hasMany(BankAccount::class);
+    }
 }
