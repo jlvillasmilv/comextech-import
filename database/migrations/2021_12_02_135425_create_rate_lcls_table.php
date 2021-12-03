137b0839ab94e27bc1f04df345411852dc3080bc
Jorge Villasmil <keine1lust@gmail.com>
2021-12-03 08:18:15 -0400

    fix rateTransport seeeder

--- database/migrations/2021_12_02_135425_create_rate_lcls_table.php
+++ database/migrations/2021_12_02_135425_create_rate_lcls_table.php
@@ -15,14 +15,26 @@ class CreateRateLclsTable extends Migration
     {
         Schema::create('rate_lcl', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('rate_transport_id')->references('id')->on('rate_transports')->onDelete('cascade');
+            $table->foreignId('user_id')->index();
+            $table->string('agent_name',50)->nullable();
+            $table->string('shipping_line',50)->nullable();
+            $table->string('coloader',50)->nullable();
+            $table->string('contract',50)->nullable();
+            $table->string('from',10)->nullable();
+            $table->string('to',10)->nullable();
+            $table->string('via',50)->nullable();
+            $table->unsignedInteger('t_time')->nullable();
+            $table->string('currency',3)->nullable();
+            $table->date('valid_from')->nullable();
+            $table->date('valid_to')->nullable();
             $table->unsignedDecimal('MIN_0_5', $precision = 8, $scale = 2);
-            $table->unsignedDecimal('0_5_TON_M3', $precision = 8, $scale = 2);
+            $table->unsignedDecimal('w0_5_TON_M3', $precision = 8, $scale = 2);
             $table->unsignedDecimal('MIN_5_10', $precision = 8, $scale = 2);
-            $table->unsignedDecimal('5_10_TON_M3', $precision = 8, $scale = 2);
+            $table->unsignedDecimal('w5_10_TON_M3', $precision = 8, $scale = 2);
             $table->unsignedDecimal('MIN_10_5', $precision = 8, $scale = 2);
-            $table->unsignedDecimal('10_15_TON_M3', $precision = 8, $scale = 2);
-            $table->unsignedDecimal('gl', $precision = 8, $scale = 2);
+            $table->unsignedDecimal('w10_15_TON_M3', $precision = 8, $scale = 2);
+            $table->unsignedDecimal('gl', $precision = 8, $scale = 2)->default(0);
+            $table->boolean('status')->default(true);
         });
     }
 
