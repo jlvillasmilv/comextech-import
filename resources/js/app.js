9c0115486ee6d628970c349e792887f199febfe7
Isaac Mendoza <isaacgtr2000@gmail.com>
2022-02-15 13:14:37 -0400

    add new component index.vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,25 +2,24 @@ window.$ = window.jQuery = require('jquery');
 require('./bootstrap');
 
 import Import from './pages/Import';
-import FreightQuote from './components/FreightQuote/Addresses';
+import FreightQuote from './components/FreightQuote/index';
 
 import Vue from 'vue';
 import store from './store';
-
 
 //Import Sweetalert2
 import Swal from 'sweetalert2';
 window.Swal = Swal;
 const Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: toast => {
-        toast.addEventListener('mouseenter', Swal.stopTimer);
-        toast.addEventListener('mouseleave', Swal.resumeTimer);
-    }
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer);
+    toast.addEventListener('mouseleave', Swal.resumeTimer);
+  }
 });
 window.Toast = Toast;
 
@@ -44,20 +43,20 @@ window.Form = Form;
 //date time Luxon
 import VueLuxon from 'vue-luxon';
 Vue.use(VueLuxon, {
-    templates: {},
-    input: {
-        zone: 'utc',
-        format: 'iso'
-    },
-    output: {
-        zone: 'local',
-        format: 'date_huge',
-        locale: null,
-        relative: {
-            round: true,
-            unit: null
-        }
+  templates: {},
+  input: {
+    zone: 'utc',
+    format: 'iso'
+  },
+  output: {
+    zone: 'local',
+    format: 'date_huge',
+    locale: null,
+    relative: {
+      round: true,
+      unit: null
     }
+  }
 });
 
 import VueLoading from 'vue-loading-overlay';
@@ -83,13 +82,13 @@ import VueNumeric from 'vue-numeric';
 Vue.use(VueNumeric);
 
 const app = new Vue({
-    el: '#app',
-    store,
-    components: {
-        'home-import-app': Import,
-        'view-quote': Quote,
-        'menu-profile': Menu,
-        'vue-numeric': Import,
-        'freight-quote' : FreightQuote
-    }
+  el: '#app',
+  store,
+  components: {
+    'home-import-app': Import,
+    'view-quote': Quote,
+    'menu-profile': Menu,
+    'vue-numeric': Import,
+    'freight-quote': FreightQuote
+  }
 });
